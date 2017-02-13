4f902059c7c34814e09d53a3926ec18c976fa775
Ben Pepper <bfpepper87@gmail.com>
2017-02-13 13:41:14 -0700

    not sure.

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,12 +1,18 @@
 class Api::V1::LinksController < ApplicationController
 
   def update
+    binding.pry
     @link = Link.find(params[:id])
     if @link.update_attributes(link_params)
       render json: @link
     else
       render json: @link.errors.full_messages, status: 500
     end
+  end
+
+  def index
+    binding.pry
+    render json: Link.where(read: true)
   end
 
   private
